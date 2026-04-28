1e9af45d2547840a806b41d0b4825d8018a6327a
Coder-X15 <samrubenabraham@yahoo.com>
2026-04-28 13:36:41 +0530

    Testing file retrieval - 19.2

--- train/traincaller.py
+++ train/traincaller.py
@@ -26,7 +26,7 @@ def setup() -> None:
 
 def train_mock() -> None:
     print("This is a mock training function")
-    command("echo 'Trained' >> trained.txt")
+    command("echo 'Trained' >> Diett/train.txt")
 
 if __name__ == "__main__":
     setup()    
