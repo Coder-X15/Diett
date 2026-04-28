f1ad001dfbab7ec6f26ffdf0f7f4e1c1d40add51
Coder-X15 <samrubenabraham@yahoo.com>
2026-04-28 11:45:07 +0530

    Testing file retrieval - 13

--- train/train.py
+++ train/train.py
@@ -10,10 +10,10 @@ import sys
 from pathlib import Path
 
 # Add the project root to Python path
-project_root = Path(__file__).parent.parent
-sys.path.insert(0, str(project_root))
-from .model import get_model, train, evaluate
-from .dataloader import get_dataloader
+train_root = Path(__file__).parent
+sys.path.insert(0, str(train_root))
+from model import get_model, train, evaluate
+from dataloader import get_dataloader
 
 dotenv.load_dotenv()
 
